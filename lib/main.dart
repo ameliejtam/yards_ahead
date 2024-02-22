99f48f79f0a998a9e7063e31ac48de461b00faa8
zhochrist <140863680+zhochrist@users.noreply.github.com>
2024-02-22 12:09:24 -0500

    Added an adaptable navigation rail
    Finished navigation rail page of the flutter tutorial!

--- lib/main.dart
+++ lib/main.dart
@@ -50,66 +50,119 @@ class MyAppState extends ChangeNotifier {
 }
 
 
-class MyHomePage extends StatelessWidget {
+// ...
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;     // ← Add this property.
+
+  @override
+  Widget build(BuildContext context) {
+    // ...
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    // ...
+    return LayoutBuilder(
+      builder: (context, contraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: contraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    // like button icon
     IconData icon;
     if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
-    }
-    else {
+    } else {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [
-            Text('A random AWESOME idea:'),
-            // box with word pair 
-            BigCard(pair: pair), 
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // like button
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                  }, 
-                  icon: Icon(icon), 
-                  label: Text('Like'), 
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromRGBO(243, 251, 236, 1), 
-                  ),
-                ),
-
-                SizedBox(width:10), 
-
-                // next button
-                ElevatedButton( 
-                  onPressed: () { 
-                    appState.getNext();
-                  }, 
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromRGBO(243, 251, 236, 1), 
-                  ),
-                  child: Text('Next'),
-                ), 
-              ],
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
 
+// ...
 // ...
 
 class BigCard extends StatelessWidget {
