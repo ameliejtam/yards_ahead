b63b61ade656f6ebc64f095eedc7e318a13d9b9b
Amelie Tam <ameliejtam@gmail.com>
2024-02-23 10:32:02 -0500

    classes were split to different files

---
+++ lib/home.dart
@@ -0,0 +1,72 @@
+part of 'main.dart';
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: Color.fromRGBO(108, 129, 88, 1),
+    );
+
+    return Card(
+      color: Color.fromRGBO(243, 251, 236, 1),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asPascalCase, style: style),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+part 'contact.dart';
+part 'home.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -118,102 +121,5 @@ class _MyHomePageState extends State<MyHomePage> {
 } 
 
 
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: Color.fromRGBO(108, 129, 88, 1),
-    );
-
-    return Card(
-      color: Color.fromRGBO(243, 251, 236, 1),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(pair.asPascalCase, style: style),
-      ),
-    );
-  }
-}
-
-
-
-class ContactPage extends StatelessWidget {
-  @override 
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-    return Center(
-      child: Text('No favorites yet!'),
-    );
-  }
-
-  return ListView(
-    children: [
-      Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text('You have ' '${appState.favorites.length} favorites:'),
-      ),
-      for (var pair in appState.favorites)
-        ListTile(
-          leading: Icon(Icons.favorite),
-          title: Text(pair.asLowerCase),
-        )
-      ],
-    );
-  }
-}
