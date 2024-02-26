9f18e01f47f6f263bd96a85e35ee60986e03ded7
zhochrist <140863680+zhochrist@users.noreply.github.com>
2024-02-26 14:18:35 -0500

    horizontal hamburger menu added

--- lib/main.dart
+++ lib/main.dart
@@ -62,80 +62,130 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// class _MyHomePageState extends State<MyHomePage> {
+//   var selectedIndex = 0;    
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // ...
+
+//     Widget page;
+//     switch (selectedIndex) {
+//       case 0:
+//         page = GeneratorPage();
+//         break;
+//       case 1:
+//         page = ContactPage();
+//         break;
+//       case 2:
+//         page = MapPage();
+//         break;
+//       case 3:
+//         page = CalendarPage();
+//         break;
+//       default:
+//         throw UnimplementedError('no widget for $selectedIndex');
+//     }
+
+//     // ...
+//     return LayoutBuilder(
+//       builder: (context, contraints) {
+//         return Scaffold(
+//           body: Row(
+//             children: [
+//               SafeArea(
+//                 child: NavigationRail(
+//                   extended: contraints.maxWidth >= 600,
+//                   destinations: [
+//                     NavigationRailDestination(
+//                       icon: Icon(Icons.home),
+//                       label: Text('Home'),
+//                     ),
+//                     NavigationRailDestination(
+//                       icon: Icon(Icons.book),
+//                       label: Text('Contacts'),
+//                     ),
+//                     NavigationRailDestination(
+//                       icon: Icon(Icons.map),
+//                       label: Text('Map'),
+//                     ),
+//                     NavigationRailDestination(
+//                       icon: Icon(Icons.calendar_month),
+//                       label: Text('Calendar'),
+//                     ),
+//                   ],
+//                   selectedIndex: selectedIndex,
+//                   onDestinationSelected: (value) {
+//                     setState(() {
+//                       selectedIndex = value;
+//                     });
+//                   },
+//                 ),
+//               ),
+//               Expanded(
+//                 child: Container(
+//                   color: Theme.of(context).colorScheme.primaryContainer,
+//                   child: page,
+//                 ),
+//               ),
+//             ],
+//           ),
+//         );
+//       }
+//     );
+//   }
+// } 
+
+
+
 class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;    
+  int _selectedTab = 0;
+
+  List _pages = [
+    Center(
+      child: GeneratorPage(),
+    ),
+    Center(
+      child: ContactPage(),
+    ),
+    Center(
+      child: MapPage(),
+    ),
+    Center(
+      child: CalendarPage(),
+    ),
+
+  ];
+
+  _changeTab(int index) {
+    setState(() {
+      _selectedTab = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // ...
-
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-        break;
-      case 1:
-        page = ContactPage();
-        break;
-      case 2:
-        page = MapPage();
-        break;
-      case 3:
-        page = CalendarPage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    // ...
-    return LayoutBuilder(
-      builder: (context, contraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: contraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.book),
-                      label: Text('Contacts'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.map),
-                      label: Text('Map'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.calendar_month),
-                      label: Text('Calendar'),
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                  },
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
-            ],
-          ),
-        );
-      }
+    return Scaffold(
+      appBar: AppBar(),
+      body: _pages[_selectedTab],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: (index) => _changeTab(index),
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Contacts"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.map), label: "Map"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month), label: "Calendar"),
+        
+        ],
+      ),
     );
   }
-} 
-
+}
 
 
 
