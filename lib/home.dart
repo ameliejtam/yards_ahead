c2c4340091d25c0f949c3ab7d0271ccf7089f240
zhochrist <140863680+zhochrist@users.noreply.github.com>
2024-04-17 12:16:37 -0400

    Tutorial Homepage Complete!

--- lib/home.dart
+++ lib/home.dart
@@ -3,53 +3,53 @@ part of 'main.dart';
 class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
 
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
+    return Scaffold(
+      //Create button to add new events
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Dialogue to tell user to input an event name
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                scrollable: true,
+                title: Text("Features"),
+                content: Padding(
+                  padding: EdgeInsets.all(2),
+                  child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+            Image.asset("assets/images/picture 1.png"), 
+            Image.asset("assets/images/pciture 2.png"),
+            Image.asset("assets/images/picture 3.png")
+            ],
+          ),
+                ),
+                //Add a submit button to the add event floating button screen
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                  }, 
+                  child: Text("Return"),
+                  )
+                ],
+              );
+            });
+        }, 
+        child: Icon(Icons.question_mark),
+      ),
+      body: Center(
       child: Column(
+        
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
             Image.asset("assets/images/unnamed.png"), 
             Image.asset("assets/images/evergreen-shrubs-boxwood.jpg"),
             ],
           ),
+    )
     );
 }
 }
-
-
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: Color.fromRGBO(108, 129, 88, 1),
-    );
-
-    return Card(
-      color: Color.fromRGBO(164, 195, 133, 1),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text("Yards Ahead"),
-      ),
-    );
-  }
-}
-
-
